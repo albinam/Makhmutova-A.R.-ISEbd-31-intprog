9ca05d8082ad0d953e6f2cb13e600343275f75b4
Albina <47570843+albinam@users.noreply.github.com>
2020-12-10 13:40:32 +0300

    site.js

--- js/site.js
+++ js/site.js
@@ -1,1 +1,31 @@
+const form = document.forms[0];
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const values = [...new FormData(form).values()];
+    console.log(values);
+
+    if (!checkIfNotNull(values[0])) return showMessage('error', 'Заполните поле ФИО');
+    if (!checkPhone(values[1])) return showMessage('error', 'Неверно введен номер телефона');
+    if (!checkIfNotNull(values[2])) return showMessage('error', 'Заполните поле адрес');
+
+    showMessage('success', 'Заказ оформлен');
+});
+
+function checkPhone(phone) {
+    var reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+    return phone != "" && reg.test(phone);
+}
+
+function checkIfNotNull(str) {
+    return str !== '';
+}
+
+function showMessage(type, text) {
+    Swal.fire({
+        text: text,
+        icon: type,
+        showConfirmButton: false
+    });
+}
